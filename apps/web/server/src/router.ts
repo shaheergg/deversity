9a368e67a8afd6c6091a71f7e553f28f4a8b2789
shaheersystems <shaheer.imran1429@gmail.com>
2024-03-03 21:05:56 +0500

    Error handler middleware added

--- apps/web/server/src/router.ts
+++ apps/web/server/src/router.ts
@@ -1,8 +1,18 @@
 import { Router } from "express";
 import { createAdmin, getAdmin } from "./controllers/admin";
+import { body } from "express-validator";
+import { errorHandler } from "./middlewares/errorHandler";
+
 const router = Router();
 
 router.get("/admin", getAdmin);
-router.post("/admin", createAdmin);
+router.post(
+  "/admin",
+  body("name").isString(),
+  body("address").isString(),
+  body("phone").isString(),
+  errorHandler,
+  createAdmin
+);
 
 export default router;
