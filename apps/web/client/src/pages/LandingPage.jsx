91cb526ecc81fe81afe77daa13eee5d3e3bb2a83
shaheersystems <shaheer.imran1429@gmail.com>
2024-03-23 22:05:28 +0500

    front-end setup

---
+++ apps/web/client/src/pages/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <section>
+      <div className="flex items-center justify-between p-4 border-b">
+        <div>
+          <h2 className="font-mono text-4xl font-semibold">Deversity.</h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default LandingPage;
---
+++ apps/web/client/src/routes/EducatorRoute.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EducatorRoute = () => {
+  return <div></div>;
+};
+
+export default EducatorRoute;
